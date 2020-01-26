f54b4012c82f21ebac5560c188023e07be534f5d
gedhean <gedhean.dev@gmail.com>
2020-01-26 00:34:13 -0300

    did resistor color challenge

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,13 @@
+export const colorCode = (color) => {
+  if (color == 'white') {
+    return 9
+  }
+
+  if (color == 'orange') {
+    return 3
+  }
+
+  return 0
+};
+
+export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
